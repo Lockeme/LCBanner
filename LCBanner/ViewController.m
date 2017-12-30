11a07ae75aefa347d1979d5c52d92cd9f946e5e0
Shaofeng <marley92@163.com>
2017-12-30 23:35:07 +0800

    Initial Commit

---
+++ LCBanner/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LCBanner
+//
+//  Created by Lockeme on 2017/12/30.
+//  Copyright © 2017年 Lockeme. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
